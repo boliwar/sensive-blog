6f9bfefaecc8972dc2c46759f6fa9d5ad13adb11
boliwar <boliwar@gmail.com>
2022-12-20 20:00:35 +0200

    Атрибуты модели указаны в правильном порядке.

--- blog/models.py
+++ blog/models.py
@@ -35,7 +35,6 @@ class Post(models.Model):
     slug = models.SlugField('Название в виде url', max_length=200)
     image = models.ImageField('Картинка')
     published_at = models.DateTimeField('Дата и время публикации')
-    objects = PostQuerySet.as_manager()
 
     author = models.ForeignKey(
         User,
@@ -51,6 +50,8 @@ class Post(models.Model):
         'Tag',
         related_name='posts',
         verbose_name='Теги')
+
+    objects = PostQuerySet.as_manager()
 
     def __str__(self):
         return self.title
